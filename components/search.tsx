d715054dbef6c2948bd708d2c8e25c3d9a83d545
Nnadozie Okeke <n.e.okeke@outlook.com>
2020-09-28 23:44:00 +0100

    set up pages in progress

---
+++ components/search.tsx
@@ -0,0 +1,10 @@
+interface Props {}
+
+function Search(props: Props) {
+    const {} = props
+
+    return (
+    )
+}
+
+export default Search
---
+++ pages/explore.tsx
@@ -0,0 +1,3 @@
+export default function Explore() {
+    return <h1>Explore the weather for this city, swipe to change cities</h1>
+}
