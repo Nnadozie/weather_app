e903c668aa423e629457bb99174adf6b1df97f13
Nnadozie Okeke <n.e.okeke@outlook.com>
2020-10-01 22:54:05 +0100

    disabled next-pwa plugin on dev

--- pages/explore.tsx
+++ pages/explore.tsx
@@ -1,3 +1,18 @@
-export default function Explore() {
-    return <h1>Explore the weather for this city, swipe to change cities</h1>
+import Nav from '../components/nav'
+
+interface Props {}
+
+function Explore(props: Props) {
+    const {} = props
+
+    return (
+        <>
+        <h1>Explore the weather for this city, swipe to change cities</h1>
+        <Nav></Nav>
+        <span>sliding up panel future weather</span>
+        </>
+    )
+
 }
+
+export default Explore
